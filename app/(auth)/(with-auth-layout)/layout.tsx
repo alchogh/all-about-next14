418c77bf9ec207c6581f5bb9ebba0a4f45f8b50d
alchogh <alchogh01@gmail.com>
2024-03-10 18:45:41 +0900

    🎨 Active Links

--- app/(auth)/(with-auth-layout)/layout.tsx
+++ app/(auth)/(with-auth-layout)/layout.tsx
@@ -1,12 +1,46 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
+
   return (
-    <h1 className="bg-cyan-800">
-      authLayout
+    <div className="bg-cyan-800 flex  justify-around text-white">
+      {navLink.map((nav) => {
+        const isActive = pathname.startsWith(nav.href);
+        return (
+          <Link
+            href={nav.href}
+            key={nav.id}
+            className={`${isActive ? "bg-yellow-700" : ""}`}
+          >
+            {nav.name}
+          </Link>
+        );
+      })}
+
       {children}
-    </h1>
+    </div>
   );
 }
+const navLink = [
+  {
+    id: 1,
+    name: "Register",
+    href: "/register",
+  },
+  {
+    id: 2,
+    name: "Login",
+    href: "/login",
+  },
+  {
+    id: 3,
+    name: "Forgot Password",
+    href: "/forgot-password",
+  },
+];
