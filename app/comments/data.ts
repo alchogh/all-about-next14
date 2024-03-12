421ea017ded1a07e4aeccdf503fee7f406562bc8
alchogh <alchogh01@gmail.com>
2024-03-12 20:59:19 +0900

    🎨 Handling POST REQUEST

--- app/comments/data.ts
+++ app/comments/data.ts
@@ -1,14 +1,14 @@
-export const comment = [
+export const comments = [
   {
-    id:1,
-    text:'This is the first comment'
+    id: 1,
+    text: "This is the first comment",
   },
   {
-    id:2,
-    text:'This is the second comment'
+    id: 2,
+    text: "This is the second comment",
   },
   {
-    id:3,
-    text:'This is the third comment'
-  }
-]
+    id: 3,
+    text: "This is the third comment",
+  },
+];
--- app/comments/route.ts
+++ app/comments/route.ts
@@ -1,5 +1,21 @@
-import { comment } from "./data"
+import { comments } from "./data";
 
-export async function GET(){
-  return Response.json(comment)
+export async function GET() {
+  return Response.json(comments);
+}
+
+export async function POST(request: Request) {
+  const comment = await request.json();
+  const newComment = {
+    id: comments.length + 1,
+    text: comment.text,
+  };
+  console.log(comment);
+  comments.push(newComment);
+  return new Response(JSON.stringify(newComment), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    status: 201,
+  });
 }
