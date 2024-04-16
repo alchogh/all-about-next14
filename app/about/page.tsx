e92fbf54063ea929b0e1f30b160c46fffd1f1c0f
alchogh <alchogh01@gmail.com>
2024-04-16 16:44:20 +0900

    🎨 Streaming & suspense 적용

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,18 @@
-import React from "react";
+import { Suspense } from "react";
+import { Reviews } from "@/components/reviews";
+import { cookies } from "next/headers";
 
 export default function About() {
-  return <div>About</div>;
+  const cookieStore = cookies();
+  const theme = cookieStore.get("theme");
+  console.log(theme);
+  console.log("about server");
+  return (
+    <h1>
+      about page {new Date().toLocaleDateString()}
+      <Suspense fallback={<p>loading</p>}>
+        <Reviews />
+      </Suspense>
+    </h1>
+  );
 }
